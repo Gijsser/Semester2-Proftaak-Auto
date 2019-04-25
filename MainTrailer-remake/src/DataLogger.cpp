ace83cc6dc68d44191268633421f9b8eccf021ca
Gijsser <45876575+Gijsser@users.noreply.github.com>
2019-04-25 15:02:57 +0200

    Ciao eerste

---
+++ MainTrailer-remake/src/DataLogger.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <SD.h>
+#include <SPI.h>
+
+#include "DataLogger.h"
+
+void datalog_log_servo(String data){
+
+}
+
+void datalog_log_sensor(String data){
+
+}
---
+++ MainTrailer-remake/src/DataLogger.h
@@ -0,0 +1,7 @@
+#ifndef _DATALOGGER_H
+#define _DATALOGGER_H
+
+void datalog_log_sensor();
+void datalog_log_servo();
+
+#endif

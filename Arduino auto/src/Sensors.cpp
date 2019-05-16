73e94364861c72662799f095c685f237db1ea41a
brianswallme <brian.vullers@home.nl>
2019-05-16 15:10:32 +0200

    add ConStatus to Steeringwheel

--- Arduino auto/src/Sensors.cpp
+++ Arduino auto/src/Sensors.cpp
@@ -9,7 +9,7 @@ unsigned long timerstreeringwheel=0;
 void get_steeringwheel_position(){
   int newPosition = analogRead(STEERING_WHEEL);
   newPosition = map(newPosition, 0, 1023, 0, 180);
-  if ((newPosition != lastPosition)&&(millis()-timerstreeringwheel>500)){
+  if ((newPosition != lastPosition)&&(millis()-timerstreeringwheel>500)&&ConStatus == OK){
     communication_send_message("WHEEL_POS", newPosition, BOTH);
     lastPosition = newPosition;
     timerstreeringwheel = millis();

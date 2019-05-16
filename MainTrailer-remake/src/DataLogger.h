07a4e254e8be118617371b25727c4d655cf96d8a
brianswallme <brian.vullers@home.nl>
2019-05-16 13:32:46 +0200

    remove
    remove file

--- MainTrailer-remake/src/DataLogger.h
+++
@@ -1,7 +0,0 @@
-#ifndef _DATALOGGER_H
-#define _DATALOGGER_H
-
-void datalog_log_sensor();
-void datalog_log_servo();
-
-#endif
--- src/Sensors.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SENSORS_H
-#define _SENSORS_H
-
-#define STEERING_WHEEL (A0)
-
-void get_steeringwheel_position();
-
-#endif

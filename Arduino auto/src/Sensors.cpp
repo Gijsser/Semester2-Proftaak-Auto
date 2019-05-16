8007bf22d8e1ca875241c67c4fd8b3bf25aea9bf
brianswallme <brian.vullers@home.nl>
2019-05-16 13:27:04 +0200

    add map voor analog read voor minder spam door vaerandering waarde

--- Arduino auto/src/Sensors.cpp
+++ Arduino auto/src/Sensors.cpp
@@ -4,11 +4,14 @@
 #include "Communication.h"
 
 int lastPosition = 0;
+unsigned long timerstreeringwheel=0;
 
 void get_steeringwheel_position(){
   int newPosition = analogRead(STEERING_WHEEL);
-  if (newPosition != lastPosition){
+  newPosition = map(newPosition, 0, 1023, 0, 180);
+  if ((newPosition != lastPosition)&&(millis()-timerstreeringwheel>500)){
     communication_send_message("WHEEL_POS", newPosition, BOTH);
     lastPosition = newPosition;
+    timerstreeringwheel = millis();
   }
 }
